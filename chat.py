7df513d8fb05b8fdd321599ec2d0d5cd74925926
Paweł Walenda <pawel.walenda@gmail.com>
2023-09-06 15:54:29 +0200

    Create Text Frame and Add Text Wiget To Get ChatGPT Responses

--- chat.py
+++ chat.py
@@ -14,3 +14,20 @@ root.iconbitmap("ai_lt.ico") # https://tkinter.com/images/ai_lt.ico
 # Set Color Scheme
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
+
+# Create Text Frame
+text_frame = customtkinter.CTkFrame(root)
+text_frame.pack(pady=20)
+
+# Add Text Wiget To Get ChatGPT Responses
+my_text = Text(text_frame,
+               bg="#343638",
+               width=65,
+               bd=1,
+               fg="#d6d6d6")
+my_text.grid(row=0, column=0)
+
+
+
+
+root.mainloop()
