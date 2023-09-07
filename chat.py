7859cff3a15bd8402cc34efb846cf1e418b77241
Paweł Walenda <pawel.walenda@gmail.com>
2023-09-07 09:43:36 +0200

    Entry widget to type stuff to ChatGPT

--- chat.py
+++ chat.py
@@ -39,5 +39,15 @@ text_scroll.grid(row=0, column=1, sticky="ns")
 # Add the scrollbar to the textbox
 my_text.configure(yscrollcommand=text_scroll.set)
 
+# Entry widget to type stuff to ChatGPT
+chat_entry = customtkinter.CTkEntry(
+    root,
+    placeholder_text="Type Something to ChatGPT ...",
+    width=535,
+    height=50,
+    border_width=2,
+)
+chat_entry.pack(pady=10)
+
 
 root.mainloop()
