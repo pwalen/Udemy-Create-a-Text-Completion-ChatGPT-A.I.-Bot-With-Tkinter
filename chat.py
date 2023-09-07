5026c324040eff8a296f02d7fdc8b4cf5b384e74
Paweł Walenda <pawel.walenda@gmail.com>
2023-09-07 08:35:18 +0200

    Add the scrollbar to the textbox

--- chat.py
+++ chat.py
@@ -9,7 +9,7 @@ import pickle
 root = customtkinter.CTk()
 root.title("ChatGPT Bot")
 root.geometry("600x600")
-root.iconbitmap("ai_lt.ico") # https://tkinter.com/images/ai_lt.ico
+root.iconbitmap("ai_lt.ico")  # https://tkinter.com/images/ai_lt.ico
 
 # Set Color Scheme
 customtkinter.set_appearance_mode("dark")
@@ -20,23 +20,24 @@ text_frame = customtkinter.CTkFrame(root)
 text_frame.pack(pady=20)
 
 # Add Text Wiget To Get ChatGPT Responses
-my_text = Text(text_frame,
-               bg="#343638",
-               width=65,
-               bd=1,
-               fg="#d6d6d6",
-               relief=FLAT,
-               wrap=WORD,
-               selectbackground="#1f538d")
+my_text = Text(
+    text_frame,
+    bg="#343638",
+    width=65,
+    bd=1,
+    fg="#d6d6d6",
+    relief=FLAT,
+    wrap=WORD,
+    selectbackground="#1f538d",
+)
 my_text.grid(row=0, column=0)
 
 # Create Scrollbar for text widget
-text_scroll = customtkinter.CTkScrollbar(text_frame,
-                                         command=my_text.yview)
+text_scroll = customtkinter.CTkScrollbar(text_frame, command=my_text.yview)
 text_scroll.grid(row=0, column=1, sticky="ns")
 
-
-
+# Add the scrollbar to the textbox
+my_text.configure(yscrollcommand=text_scroll.set)
 
 
 root.mainloop()
